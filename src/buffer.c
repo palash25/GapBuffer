55bd860513d15bb73d3608285ef96d5c2e7ca94a
Palash Nigam <npalash25@gmail.com>
2019-09-01 17:30:43 +0530

    Add resize function

--- src/buffer.c
+++ src/buffer.c
@@ -19,8 +19,8 @@
 */
 buffer* init(int size) {
     buffer* buf = malloc(sizeof(buffer));
-    buf->arr = (char*)calloc((size_t)buf->size, sizeof(char));
     buf->size = size;
+    buf->arr = (char*)calloc((size_t)buf->size, sizeof(char));
     buf->start = (int*)&buf->arr[0];
     buf->end = (int*)&buf->arr[buf->size - 1];
     // initially the cursor spreads over the entirity of the buffer
@@ -35,7 +35,7 @@ buffer* init(int size) {
 // character at the beginning of the rear end to the
 // end of the front end of the buffer.
 void move_cursor_forward(buffer *buf) {
-    printf("==========AFTER MOVING FORWARD=========\n");
+    printf("==========BEFORE MOVING FORWARD=========\n");
     printf("CS: %d\tCE: %d\n", buf->cursor_start, buf->cursor_end);
 
     char *pointer = buf->cursor_end +1;
@@ -47,13 +47,15 @@ void move_cursor_forward(buffer *buf) {
 
     printf("%c\t%d\t%s\n", (*buf->cursor_start), buf->cursor_start, buf->arr);
     buff_print(buf);
+    printf("==========AFTER MOVING FORWARD=========\n");
+    printf("CS: %d\tCE: %d\n", buf->cursor_start, buf->cursor_end);
 }
 
 // moves the cursor one location backward and copies
 // character at the end of the front end to the
 // beginning of the rear end of the buffer.
 void move_cursor_backward(buffer *buf) {
-    printf("==========AFTER MOVING BACKWARD=========\n");
+    printf("==========BEFORE MOVING BACKWARD=========\n");
     printf("CS: %d\tCE: %d\n", buf->cursor_start, buf->cursor_end);
 
     char *pointer = buf->cursor_start -1;
@@ -65,6 +67,8 @@ void move_cursor_backward(buffer *buf) {
 
     printf("%c\t%d\t%s\n", (*buf->cursor_start), buf->cursor_start, buf->arr);
     buff_print(buf);
+    printf("==========AFTER MOVING BACKWARD=========\n");
+    printf("CS: %d\tCE: %d\n", buf->cursor_start, buf->cursor_end);
     printf("BACKWARD BUFF SIZE: %d GAP SIZE: %d\n", buf->size, buf->gap_size);
 }
 
@@ -74,8 +78,10 @@ void insert_str(buffer *buf, char *str) {
     printf("==========AFTER INSERTING STRING=========\n");
     for(int i=0; i<strlen(str); i++) {
         printf("run #%d\n", i);
-        *buf->cursor_start++ = str[i];
-        if(buf->cursor_start == buf->cursor_end) {
+        *buf->cursor_start = str[i];
+        printf("Start: %d\t CS: %d\tCE: %d\t character: %c\n", buf->start, buf->cursor_start, buf->cursor_end, *buf->cursor_start);
+        buf->cursor_start++;
+        if(buf->cursor_start > buf->cursor_end) {
             resize(buf);
         } else {
             printf("STILL NOT EQ; CS: %d\tCE: %d\n", buf->cursor_start, buf->cursor_end);
@@ -83,6 +89,30 @@ void insert_str(buffer *buf, char *str) {
     }
     printf("\nINSERT BUFF SIZE: %d GAP SIZE: %d\n", buf->size, buf->gap_size);
 }
+
+// insert a gap twice the size of the previous gap at the cursor.
+void resize(buffer *buf) {
+    printf("==========AFTER RESIZING=========\n");
+    printf("BUFF SIZE: %d GAP SIZE: %d\n", buf->size, buf->gap_size);
+    printf("CURSOR START: %d CURSOR END: %d\n", buf->cursor_start, buf->cursor_end);
+
+    int *res = realloc(buf->arr, (buf->gap_size * 2) * sizeof(char));
+    if(res) {
+        buf->gap_size *= 2;
+        buf->size += buf->gap_size;
+        buf->end += buf->gap_size;
+        buf->cursor_start++;
+        buf->cursor_end = &buf->arr[(buf->size - 1)];
+        printf("BUFF SIZE: %d GAP SIZE: %d\n", buf->size, buf->gap_size);
+        printf("AFTER RESIZE  bufStart: %d\t cStart: %d\t cEnd: %d\t bufEnd: %d\n", buf->start, buf->cursor_start, buf->cursor_end, buf->end);
+        buff_print(buf);
+        return;
+    } else {
+        fprintf(stderr, "ERROR: realloc failed");
+        return;
+    }
+}
+
 
 void buff_print(buffer *buf) {
     for(int i=0; i<buf->size; i++) {
@@ -92,4 +122,5 @@ void buff_print(buffer *buf) {
             printf("%c\n", buf->arr[i]);
         }
     }
+    printf("\n");
 }
